3579ab5083ebe00dc828408288c22d39e43f5a4f
alxqiu <alxqiu@gmail.com>
2023-02-19 21:35:54 -0500

    adding small margin to bottom of page

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -4,7 +4,7 @@ import ExploreComponent from "./ExploreComponent.js";
 
 function exploreComponent() {
     $('#wd-explore').append(`
-        <div class="row mt-2">
+        <div class="row mt-2 mb-2">
             <!-- Right Column -->
             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
                 ${NavigationSidebar()}
